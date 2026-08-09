9cbca5654aefe0f3f57418d5ac73bb7520850307
Michal Trojanowski <mtroja98@gmail.com>
2026-08-09 08:26:56 +0200

    Send BanDto when user is banned

--- src/main/kotlin/site/komuna/reserve/user/model/UserDto.kt
+++ src/main/kotlin/site/komuna/reserve/user/model/UserDto.kt
@@ -1,28 +1,28 @@
 package site.komuna.reserve.user.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import site.komuna.reserve.user.Role
+import site.komuna.reserve.user.ban.model.BanDto
 import java.time.OffsetDateTime
 
-class UserDto(
+@JsonInclude(JsonInclude.Include.NON_NULL) // nie wysylaj banDto jesli nie jest zbanowany
+data class UserDto(
     val id: Long,
     val email: String,
     val nick: String,
     val role: Role,
-    var trusted: Boolean = false,
-    var banned: Boolean = false,
-    var bannedUntil: OffsetDateTime? = null
+    val trusted: Boolean = false,
+    val banDto: BanDto? = null
 ) {
     constructor(
         userEntity: UserEntity,
-        banned: Boolean = false,
-        bannedUntil: OffsetDateTime? = null
+        banDto: BanDto? = null
     ) : this(
         id = userEntity.id!!,
         email = userEntity.email,
         nick = userEntity.nick,
         role = userEntity.role,
         trusted = userEntity.trusted,
-        banned = banned,
-        bannedUntil = bannedUntil
+        banDto = banDto
     )
 }
